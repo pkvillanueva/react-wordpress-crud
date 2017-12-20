b7e6e711b4af0d5d8b4da76d0391ada615832cfa
Patrick Villanueva <pkrvillanueva@gmail.com>
2017-12-20 18:44:01 +0800

    Simplified registering post types

--- content/plugins/rwc-backend/includes/class-rwc-backend-post-types.php
+++ content/plugins/rwc-backend/includes/class-rwc-backend-post-types.php
@@ -20,36 +20,24 @@ class RWC_Backend_Post_Types {
 	 * @since 1.0.0
 	 */
 	public function register_post_types() {
-		/** Duplicate and call it here. */
-		$this->register_applicant_post_type();
-	}
-
-	/**
-	 * Register applicant post type
-	 * 
-	 * @since 1.0.0
-	 */
-	public function register_applicant_post_type() {
-		/** Post type labels. */
-		$labels = array(
-			'name'               => _x( 'Applicants', 'Post type general name', 'rwc-backend' ),
-			'singular_name'      => _x( 'Applicant', 'Post type singular name', 'rwc-backend' ),
-			'menu_name'          => _x( 'Applicants', 'Admin Menu text', 'rwc-backend' ),
-			'name_admin_bar'     => _x( 'Applicant', 'Add New on Toolbar', 'rwc-backend' ),
-			'add_new'            => __( 'Add New', 'rwc-backend' ),
-			'add_new_item'       => __( 'Add New Applicant', 'rwc-backend' ),
-			'new_item'           => __( 'New Applicant', 'rwc-backend' ),
-			'edit_item'          => __( 'Edit Applicant', 'rwc-backend' ),
-			'view_item'          => __( 'View Applicant', 'rwc-backend' ),
-			'all_items'          => __( 'All Applicants', 'rwc-backend' ),
-			'search_items'       => __( 'Search Applicants', 'rwc-backend' ),
-			'parent_item_colon'  => __( 'Parent Applicants:', 'rwc-backend' ),
-			'not_found'          => __( 'No applicants found.', 'rwc-backend' ),
-			'not_found_in_trash' => __( 'No applicants found in Trash.', 'rwc-backend' )
-		);
-	
-		$args = array(
-			'labels'             => $labels,
+		/** Post type - applicant. */
+		register_post_type( 'applicant', array(
+			'labels'             => array(
+				'name'               => _x( 'Applicants', 'Post type general name', 'rwc-backend' ),
+				'singular_name'      => _x( 'Applicant', 'Post type singular name', 'rwc-backend' ),
+				'menu_name'          => _x( 'Applicants', 'Admin Menu text', 'rwc-backend' ),
+				'name_admin_bar'     => _x( 'Applicant', 'Add New on Toolbar', 'rwc-backend' ),
+				'add_new'            => __( 'Add New', 'rwc-backend' ),
+				'add_new_item'       => __( 'Add New Applicant', 'rwc-backend' ),
+				'new_item'           => __( 'New Applicant', 'rwc-backend' ),
+				'edit_item'          => __( 'Edit Applicant', 'rwc-backend' ),
+				'view_item'          => __( 'View Applicant', 'rwc-backend' ),
+				'all_items'          => __( 'All Applicants', 'rwc-backend' ),
+				'search_items'       => __( 'Search Applicants', 'rwc-backend' ),
+				'parent_item_colon'  => __( 'Parent Applicants:', 'rwc-backend' ),
+				'not_found'          => __( 'No applicants found.', 'rwc-backend' ),
+				'not_found_in_trash' => __( 'No applicants found in Trash.', 'rwc-backend' )
+			),
 			'description'        => __( 'Use to store applicants information.', 'rwc-backend' ),
 			'public'             => false,
 			'publicly_queryable' => true,
@@ -66,8 +54,6 @@ class RWC_Backend_Post_Types {
 			'supports'           => array( 'title', 'custom-fields' ),
 			'show_in_rest'       => true,
 			'rest_base'          => 'applicants'
-		);
-	
-		register_post_type( 'applicant', $args );
+		) );
 	}
 }
