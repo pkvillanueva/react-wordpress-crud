1a3e4759f02a0b6122db5ac2aea141d541181cac
Patrick Villanueva <pkrvillanueva@gmail.com>
2017-12-22 17:43:54 +0800

    Revert folder component for App

--- content/themes/rwc-frontend/src/components/App.js
+++ content/themes/rwc-frontend/src/components/App.js
@@ -3,7 +3,7 @@ import './style.css';
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Applicants from '../Applicants';
+import Applicants from './Applicants';
 
 class App extends Component {
 	render() {
