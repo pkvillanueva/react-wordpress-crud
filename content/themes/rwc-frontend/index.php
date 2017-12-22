b6850ba88767fd117c98438bb46d5b8c7a804758
Patrick Villanueva <pkrvillanueva@gmail.com>
2017-12-22 18:51:36 +0800

    Change from app to root for consistency

--- content/themes/rwc-frontend/index.php
+++ content/themes/rwc-frontend/index.php
@@ -10,7 +10,7 @@ get_header();
 	
 	<noscript><?php esc_html_e( 'You need to enable JavaScript to run this app.', 'rwc-frontend' ); ?></noscript>
 	
-	<div id="root" class="site__app"></div><!-- #root -->
+	<div id="root" class="site__root"></div><!-- #root -->
 
 <?php
 get_footer();
