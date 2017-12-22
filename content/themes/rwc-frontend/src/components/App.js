a3d97dbe2cfde9a3396a79c8c92a7ba1d8f8f2fa
Patrick Villanueva <pkrvillanueva@gmail.com>
2017-12-22 18:59:27 +0800

    Add new routes

--- content/themes/rwc-frontend/src/components/App.js
+++ content/themes/rwc-frontend/src/components/App.js
@@ -1,16 +1,23 @@
 import './style.css';
 
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Applicants from './Applicants';
+import ApplicantList from './Applicant/ApplicantList';
+import ApplicantNew from './Applicant/ApplicantNew';
+import ApplicantEdit from './Applicant/ApplicantEdit';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
 				<BrowserRouter>
-					<Route exact path="/" component={Applicants} />
+					<Switch>
+						<Route exact path="/" component={ApplicantList} />
+						<Route exact path="/new" component={ApplicantNew} />
+						<Route exact path="/edit/:id" component={ApplicantEdit} />
+						<Route component={ApplicantList} /> 
+					</Switch>
 				</BrowserRouter>
 			</div>
 		);
